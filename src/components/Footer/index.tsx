2e7e018b3a5ab91df76d3ebcf3546956429825cf
khattak2 <youremail@example.com>
2025-07-12 00:10:23 -0700

    Updated section / fixed bugs / added features

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,7 +11,6 @@ const Footer = () => {
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
               <div className="mb-12 max-w-[360px] lg:mb-16">
                 <Link href="/" className="mb-8 inline-block">
-{/* import Image from "next/image"; // make sure this is at the top of the file */}
 
 <Image
   src="/favicon.png"
